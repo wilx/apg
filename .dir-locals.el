118d1f6265c3f34186333ca2b926cee3b41854a0
Vaclav Zeman <vhaisman@gmail.com>
2015-01-27 18:52:16 +0100

    Add .dir-locals.el.

---
+++ .dir-locals.el
@@ -0,0 +1,18 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil
+  (fill-column . 79)
+  (indent-tabs-mode)
+  (show-trailing-whitespace . t)
+  (whitespace-style face trailing lines-tail space-before-tab indentation empty)
+  (whitespace-newline . t))
+ (c++-mode
+  (tab-width . 8)
+  (whitespace-action warn-read-only auto-cleanup))
+ (c-mode
+  (tab-width . 8)
+  (whitespace-action warn-read-only auto-cleanup))
+ (cmake-mode
+  (whitespace-action warn-read-only auto-cleanup)))
+
