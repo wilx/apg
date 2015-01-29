51cec91d486474fd4212f2135c11aa0060f623dd
Vaclav Zeman <vhaisman@gmail.com>
2015-01-29 20:25:11 +0100

    apg.c: Remove warning.

--- apg.c
+++ apg.c
@@ -478,7 +478,7 @@ main (int argc, char *argv[])
      /***************************************
      ** ALGORITHM = 1 RESTRICTIONS = PRESENT
      ****************************************/
-     if ( (restrictions_present == TRUE))
+     if (restrictions_present == TRUE)
        {
         /* Filter check */
         if (filter_restrict_present == TRUE)
